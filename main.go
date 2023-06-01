6791908d5880911cdbae78e15974b780c844d92f
bjorn <8081834+s41nn0n@users.noreply.github.com>
2023-06-01 09:57:54 +0200

    chore: initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(Message(flag.Args()...))
+}
+
+func Message(who ...string) string {
+	switch len(who) {
+	case 0:
+		return fmt.Sprint("Hello there!")
+	case 1:
+		return fmt.Sprintf("Hello %s", who[0])
+	default:
+		return fmt.Sprintf("Hello %s", strings.Join(who, ", "))
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_message(t *testing.T) {
+	type args struct {
+		who []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Basic Test",
+			args: args{
+				who: []string{"there"},
+			},
+			want: "Hello there",
+		},
+		{
+			name: "Basic Test no args",
+			want: "Hello there!",
+		},
+		{
+			name: "Basic Args Array",
+			args: args{
+				who: []string{"1", "2"},
+			},
+			want: "Hello 1, 2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Message(tt.args.who...); got != tt.want {
+				t.Errorf("message() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
